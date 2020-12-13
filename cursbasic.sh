341ca81be4b3e36e85c877ec121369261fbaf881
cmalita <cristimalita@gmail.com>
2020-12-13 23:15:27 +0200

    Update cursbasic.sh

--- cursbasic.sh
+++ cursbasic.sh
@@ -8,7 +8,8 @@ useradd ${username}
 echo ${pass} | passwd --stdin ${username} >> /dev/null
 
 #Populate the student's home dir with stuff
-HOMEDIR=$(tail -n 1 /etc/passwd | cut -d : -f 6)
+HOMEDIR=$(eval echo "~${username}")
+printf "Homedir is ${HOMEDIR}"
 printf "Downloading files\n"
 curl -s https://raw.githubusercontent.com/Bryan-Legend/babel-lang/master/Babel.EnglishEmitter/Resources/Irregular%20Verbs.txt -o ${HOMEDIR}/irregular_verbs
 chown -R ${username}:${username} ${HOMEDIR}
