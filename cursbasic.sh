88201dd027644126b15f8324dcd2571aaa47593c
cmalita <cristimalita@gmail.com>
2020-12-13 23:07:45 +0200

    Update cursbasic.sh

--- cursbasic.sh
+++ cursbasic.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+#Variables
+username="student"
+password="student"
 
-printf "Salut, o sa iti configuram linuxul pentru a putea urma acest curs. Pentru inceput, te rugam sa iti alegi un nume de utilizator. Numele poate sa contina doar caractere alfanumerice.\n"
-read -p "Nume utilizator: " username
-username=$(echo ${username} | tr '[:upper:]' '[:lower:]')
-printf "Ok, acum hai sa iti setam si o parola: \n"
-read -sp "Parola: " pass
+#Add user
 useradd ${username}
 echo ${pass} | passwd --stdin ${username} >> /dev/null
 
-#Populate the user's home dir with stuff
+#Populate the student's home dir with stuff
 HOMEDIR=$(tail -n 1 /etc/passwd | cut -d : -f 6)
-printf "Homedir is ${HOMEDIR}\n"
+printf "Downloading files\n"
 curl -s https://raw.githubusercontent.com/Bryan-Legend/babel-lang/master/Babel.EnglishEmitter/Resources/Irregular%20Verbs.txt -o ${HOMEDIR}/irregular_verbs
 chown -R ${username}:${username} ${HOMEDIR}
